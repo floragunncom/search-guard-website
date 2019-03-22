43890da23c29bc31934160e9d29357b1fa275f95
Robin Zuschke <robin.l.zuschke@gmail.com>
2019-03-22 18:38:04 +0100

    Setup i18n

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { injectIntl, defineMessages } from "react-intl";
+
+const messages = defineMessages({
+  title: {
+    id: 'app.title',
+    defaultMessage: 'Welcome to React'
+  },
+  content1: {
+    id: 'app.content1',
+    defaultMessage: 'To get started, edit'
+  },
+  content2: {
+    id: 'app.content2',
+    defaultMessage: 'and save to reload.'
+  },
+});
 
 class App extends Component {
   render() {
+    const { intl: { formatMessage } } = this.props;
     return (
       <div className="App">
         <header className="App-header">
+          <div className="languages">
+            <a href="/?locale=en">English</a>
+            <a href="/?locale=de">Deutsch</a>
+          </div>
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">{formatMessage(messages.title)}</h1>
         </header>
+        <p className="App-intro">
+          {formatMessage(messages.content1)} <code>src/App.js</code> {formatMessage(messages.content2)}
+        </p>
       </div>
     );
   }
 }
 
-export default App;
+export default injectIntl(App);
--- src/index.js
+++ src/index.js
@@ -2,11 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// i18n
+import { addLocaleData, IntlProvider } from 'react-intl';
+import deLocaleData from "react-intl/locale-data/de";
+import translations from './i18n/locales/';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+addLocaleData(deLocaleData);
+const locale = window.location.search.replace("?locale=", "") || "en";
+const messages = translations[locale];
+
+ReactDOM.render(
+  <IntlProvider 
+    locale={locale}
+    key={locale}
+    messages={messages}
+  >
+    <App />
+  </IntlProvider>
+  , document.getElementById('root')
+);
