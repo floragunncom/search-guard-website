ddbe5a0baea6c83e4ba2ad1e454b7063bd8627ca
JakobBe <jakob.bethmann@gmail.com>
2019-03-26 11:37:12 +0100

    Add routing to contact view

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import App from './App';
+import ContactUs from './views/ContactUs';
 
 const Routes = () => {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={App} />
+        <Route exact path="/contact" component={ContactUs} />
       </Switch>
     </div>
   );
