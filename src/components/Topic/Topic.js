d75882d13b3a7d3e4564539adbfaa7c524ed1846
Marcus Zierke <marcuszierke@gmail.com>
2019-04-29 21:49:33 +0200

    company view finalised

--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -2,9 +2,19 @@ import React from 'react';
 import './Topic.scss';
 
 const Topic = (props) => {
+  let icon = undefined;
+  if (props.icon) {
+    icon = (
+      <div className="topic-icon">
+        <img src={props.icon} alt="icon"/>
+      </div>
+    );
+  }
+
   return (
-    <div className="topic-wrapper">
+    <div className={props.icon ? "topic-wrapper topic-wrapper-icon" : "topic-wrapper"}>
       <div className="row">
+        {icon}
         <div className="topic-headline">{props.headline}</div>
         <div className="topic-text">{props.text}</div>
       </div>
