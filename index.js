3e23f026b066af0552cbbd7ef6d3d56ddb0baa72
Marcus Zierke <marcuszierke@gmail.com>
2019-08-12 08:58:33 +0200

    prepare push to Heroku

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 const handler = require('serve-handler');
 const http = require('http');
 
-const port = process.env.PORT || 4444
+const port = process.env.PORT || 4444;
 
 const options = {
-  public: "./build",
-  "redirects": [
-    { "source": "/old", "destination": "/new", "type": 301 }
-  ]
-}
+  public: './build',
+  redirects: [{ source: '/old', destination: '/new', type: 301 }],
+};
 
 const server = http.createServer((request, response) => {
   // You pass two more arguments for config and middleware
   // More details here: https://github.com/zeit/serve-handler#options
   return handler(request, response, options);
-})
+});
 
 server.listen(port, () => {
   console.log(`Running at http://localhost:${port}`);
