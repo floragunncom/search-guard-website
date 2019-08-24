4c005e11590cb550a2b4da3049daa845ff72d06c
Marcus Zierke <marcuszierke@gmail.com>
2019-08-24 15:22:09 +0200

    search to Blog Feed added

--- src/components/Button/ButtonDefault.js
+++ src/components/Button/ButtonDefault.js
@@ -3,9 +3,9 @@ import './ButtonDefault.scss';
 
 const ButtonLoud = props => {
   return (
-    <div className="arrow-button-default-container" onClick={props.onPress}>
+    <button className="arrow-button-default-container" onClick={props.onPress}>
       <div className="arrow-button-default-text">{props.text}</div>
-    </div>
+    </button>
   );
 };
 
--- src/views/Blog/Blog.js
+++ src/views/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import * as lunr from 'lunr';
 import NavBar from '../../components/NavBar/NavBar';
 import Title from '../../components/Title/Title';
 import PreFooter from '../../components/PreFooter/PreFooter';
@@ -6,9 +7,12 @@ import Pagination from '../../components/Pagination/Pagination';
 import Footer from '../../components/Footer/Footer';
 import client from '../../components/Client/Client';
 import BlogPost from './BlogPost';
+import SearchBlogPost from './SearchBlogPost';
 
 const Blog = () => {
   const [posts, setPosts] = useState([]);
+  const [searchResultPosts, setSearchResultPosts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [postsPerPage] = useState(10);
@@ -16,47 +20,155 @@ const Blog = () => {
   useEffect(() => {
     const fetchPosts = async () => {
       setLoading(true);
-      const fetch = await client.getEntries({ content_type: 'post', 'order':'sys.createdAt' }).then(response => {
-        console.log('response', response)
-        setPosts(response.items);
-      });
+      const fetch = await client
+        .getEntries({ content_type: 'post', order: 'sys.createdAt' })
+        .then(response => {
+          setPosts(response.items);
+        });
+      setLoading(false);
     };
-    
     fetchPosts();
-    
-    setLoading(false);
   }, []);
+
+  function onSearchTermChange(event) {
+    const searchTerm = event.target.value;
+    setSearchTerm(searchTerm);
+    const searchResultPosts = [];
+    const searchResult = searchIndex.search(searchTerm);
+    searchResult.map(res => {
+      searchResultPosts.push(posts.find(post => post.sys.id === res.ref));
+    });
+    setSearchResultPosts(searchResultPosts);
+  }
+
+  const documents = [];
+  posts.map(post => {
+    documents.push({
+      id: post.sys.id,
+      author: post.fields.author,
+      title: post.fields.title,
+      content: post.fields.postContent,
+    });
+  });
+
+  const searchIndex = lunr(function() {
+    this.ref('id');
+    this.field('content');
+    this.field('title');
+    this.field('author');
+
+    this.pipeline.remove(lunr.stemmer);
+    this.searchPipeline.remove(lunr.stemmer);
+
+    documents.forEach(doc => {
+      this.add(doc);
+    });
+  });
+
+  // function searchResult(searchTerm) {
+  //   console.log('searchTerm', searchTerm)
+  //   let searchResultPosts = [];
+  //   const searchResult = searchIndex.search(searchTerm);
+  //   searchResult.map(res => {
+  //     searchResultPosts.push(posts.find(post => post.sys.id === res.ref));
+  //   });
+  //   setSearchResultPosts(searchResultPosts);
+  //     console.log('searchResultPosts', searchResultPosts)
+  // }
 
   const indexOfLastPost = currentPage * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
   const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+  let postResult;
 
   const paginate = pageNumber => setCurrentPage(pageNumber);
 
-  const postsList = loading ? <div>Loading ...</div> : (
-    <div style={{ display: 'flex', flexDirection: 'column' }}>
-      <div>
-        {currentPosts.map((post, index) => {
-          return (
-            <div className="col s12 l6 blogpost-column-wrapper">
-              <BlogPost key={index} post={post} posts={posts} />
-            </div>
-          );
-        })}
+  if (loading) {
+    postResult = <div className="searchblogpost-no-results">Loading ...</div>;
+  } else {
+    postResult = (
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        <div className="blog-searchbar">
+          <div className="row">
+            <form>
+              <div className="row">
+                <div className="input-field col m8 offset-m2 s12 center">
+                  <i className="material-icons prefix">search</i>
+                  <input
+                    id="icon_prefix"
+                    type="text"
+                    className="validate"
+                    onChange={event => onSearchTermChange(event)}
+                    placeholder="Search blog ..."
+                  />
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div>
+          {currentPosts.map((post, index) => {
+            return (
+              <div className="col s12 l6 blogpost-column-wrapper">
+                <BlogPost key={index} post={post} posts={posts} />
+              </div>
+            );
+          })}
+        </div>
+        <Pagination
+          postsPerPage={postsPerPage}
+          totalPosts={posts.length}
+          paginate={paginate}
+        />
       </div>
-      <Pagination
-        postsPerPage={postsPerPage}
-        totalPosts={posts.length}
-        paginate={paginate}
-      />
-    </div>
-  );
+    );
+  }
+
+  if (searchTerm.length > 1) {
+    postResult = (
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        <div className="blog-searchbar">
+          <div className="row">
+            <form>
+              <div className="row">
+                <div className="input-field col m8 offset-m2 s12 center">
+                  <i className="material-icons prefix">search</i>
+                  <input
+                    id="icon_prefix"
+                    type="text"
+                    className="validate"
+                    onChange={event => onSearchTermChange(event)}
+                    placeholder="Search blog ..."
+                  />
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div>
+          {searchResultPosts.length === 0 ? (
+            <div className="searchblogpost-no-results">
+              No results for '{searchTerm}'
+            </div>
+          ) : (
+            searchResultPosts.map((post, index) => {
+              return (
+                <div className="col m12 l8 offset-l2 searchblogpost-wrapper">
+                  <SearchBlogPost key={index} post={post} />
+                </div>
+              );
+            })
+          )}
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div>
       <NavBar />
       <Title headline="Blog" />
-      <div className="row">{postsList}</div>
+      <div className="row">{postResult}</div>
       <PreFooter />
       <Footer />
     </div>
