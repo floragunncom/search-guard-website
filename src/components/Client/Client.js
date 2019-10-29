55e8bd12c67102474c55d522a9dfc5f6c6807322
Marcus Zierke <marcuszierke@gmail.com>
2019-10-29 14:40:28 +0100

    credentials hardcoded for build

--- src/components/Client/Client.js
+++ src/components/Client/Client.js
@@ -1,8 +1,11 @@
 import { createClient } from 'contentful';
 
 const client = createClient({
-  space: process.env.REACT_APP_CONTENTFUL_SPACE,
-  accessToken: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN,
+  // space: process.env.REACT_APP_CONTENTFUL_SPACE,
+  // accessToken: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN,
+  space: '95di84mqkkro',
+  accessToken:
+    'e374194597d1e72907428441d7ffe9f2ef9486dc92b23a733d9ca02d87e4da3c',
 });
 
 export default client;
