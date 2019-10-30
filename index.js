95d3adda961cb4aca13b238355eb28da489eb1f3
Marcus Zierke <marcuszierke@gmail.com>
2019-10-30 20:14:31 +0100

    fetching data locally - 2.0

--- index.js
+++ index.js
@@ -1,55 +1,64 @@
 const handler = require('serve-handler');
 const http = require('http');
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
+const data = require('./data/redirects.json');
 
-const setRedirectRule = (oldUrl, newUrl, urlStatus) => {
-  if (oldUrl && newUrl && urlStatus) {
-    if (urlStatus.gs$cell.inputValue !== 'Good') {
-      return {
-        source: `${oldUrl.gs$cell.inputValue}`,
-        destination: `${newUrl.gs$cell.$t}`,
-        type: 301,
-      };
-    }
-    return {
-      source: `${oldUrl.gs$cell.inputValue}`,
-      destination: `${oldUrl.gs$cell.inputValue}`,
-      type: 301,
-    };
-  }
-};
+// const setRedirectRule = (oldUrl, newUrl, urlStatus) => {
+//   if (oldUrl && newUrl && urlStatus) {
+//     if (urlStatus.gs$cell.inputValue !== 'Good') {
+//       return {
+//         source: `${oldUrl.gs$cell.inputValue}`,
+//         destination: `${newUrl.gs$cell.$t}`,
+//         type: 301,
+//       };
+//     }
+//     return {
+//       source: `${oldUrl.gs$cell.inputValue}`,
+//       destination: `${oldUrl.gs$cell.inputValue}`,
+//       type: 301,
+//     };
+//   }
+// };
 
-const buildOptions = async res => {
-  const resEntries = await res.feed.entry;
-  const entryListLength = await res.feed.entry[res.feed.entry.length - 1].gs$cell.row;
-  const redirectRules = [];
-  const rules = await resEntries.forEach(async (entry, index) => {
-    if (index <= entryListLength) {
-      const oldUrl = await resEntries.filter(entry => entry.gs$cell.col === '1' && entry.gs$cell.row === `${index}`)[0];
-      const urlStatus = await resEntries.filter(entry => entry.gs$cell.col === '8' && entry.gs$cell.row === `${index}`)[0];
-      const newUrl = await resEntries.filter(entry => entry.gs$cell.col === '12' && entry.gs$cell.row === `${index}`)[0];
+// const buildOptions = async res => {
+//   const resEntries = await res.feed.entry;
+//   const entryListLength = await res.feed.entry[res.feed.entry.length - 1].gs$cell.row;
+//   const redirectRules = [];
+//   const rules = await resEntries.forEach(async (entry, index) => {
+//     if (index <= entryListLength) {
+//       const oldUrl = await resEntries.filter(entry => entry.gs$cell.col === '1' && entry.gs$cell.row === `${index}`)[0];
+//       const urlStatus = await resEntries.filter(entry => entry.gs$cell.col === '8' && entry.gs$cell.row === `${index}`)[0];
+//       const newUrl = await resEntries.filter(entry => entry.gs$cell.col === '12' && entry.gs$cell.row === `${index}`)[0];
 
-      redirectRules.push(setRedirectRule(oldUrl, newUrl, urlStatus));
-    }
-  });
-  return { public: './build', redirects: redirectRules };
-};
+//       redirectRules.push(setRedirectRule(oldUrl, newUrl, urlStatus));
+//     }
+//   });
+//   return { public: './build', redirects: redirectRules };
+// };
 
-const startServer = async res => {
-  const options = await buildOptions(res);
+// const startServer = async res => {
+const startServer = async () => {
+  console.log('starting build');
+  const redirectLinks = await data.redirects.filter(link => link !== null);
+  const options = await { public: './build', redirects: redirectLinks };
+  console.log('options', options)
   const server = http.createServer((request, response) => {
     return handler(request, response, options);
   });
-
+  
   const port = process.env.PORT || 4444;
   server.listen(port, () => {
     console.log(`Running at http://localhost:${port}`);
   });
+  console.log('finished build');
 };
 
-fetch('https://spreadsheets.google.com/feeds/cells/1U_TbyRqBf6wbRXzGa63in5LexHiCKzkUWPWnqb9FbgY/2/public/full?alt=json')
-  .then(response => response.json())
-  .then(res => startServer(res));
+startServer();
+// };
+
+// fetch('https://spreadsheets.google.com/feeds/cells/1U_TbyRqBf6wbRXzGa63in5LexHiCKzkUWPWnqb9FbgY/2/public/full?alt=json')
+//   .then(response => response.json())
+//   .then(res => startServer(res));
 
 // const options = {
 //   public: './build',
