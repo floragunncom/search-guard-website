0773d96ecc5ada6f5c751ecf080932bfbd612e18
Jochen Kressin <jkressin@floragunn.com>
2019-12-20 17:30:53 +0100

    reordered team

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -17,16 +17,16 @@ const Team = () => {
       link: 'https://www.linkedin.com/in/searchguard/',
     },
     {
-      name: 'Jochen Kressin',
-      position: 'Founder & CEO',
-      pic: jochen,
-      link: 'https://www.linkedin.com/in/jkressin/',
-    },
-    {
       name: 'Claudia Kressin',
       position: 'Founder & CEO',
       pic: claudia,
       link: 'https://www.linkedin.com/in/searchguard/',
+    },
+    {
+      name: 'Jochen Kressin',
+      position: 'Founder & CEO',
+      pic: jochen,
+      link: 'https://www.linkedin.com/in/jkressin/',
     },
     {
       name: 'Hendrik Saly',
@@ -50,7 +50,7 @@ const Team = () => {
         <div className="company-team-headline">Management team</div>
         {team.map(person => {
           return (
-            <div className="col s12 m6 company-team-profile">
+            <div className="col s12 m4 company-team-profile">
               <img src={person.pic} alt="person" className="profile-pic" />
               <div className="profile-name">{person.name}</div>
               <div className="profile-position">{person.position}</div>
