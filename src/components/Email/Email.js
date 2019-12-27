c93f23c779266199cf6d3b097a79824a77d84c98
Marcus Zierke <marcuszierke@gmail.com>
2019-12-27 01:28:02 +0100

    Tracking init added to Email

--- src/components/Email/Email.js
+++ src/components/Email/Email.js
@@ -1,9 +1,14 @@
-import React, { useState } from 'react';
-import { Event } from '../Tracking/Tracking';
+import React, { useState, useEffect } from 'react';
+import { initGA, Event, PageView } from '../Tracking/Tracking';
 import './Email.scss';
 import Button from '../Button/Button';
 
 const Email = () => {
+  useEffect(() => {
+    initGA();
+    PageView();
+  }, []);
+
   const [emailSendStatus, setEmailSendStatus] = useState(false);
 
   function onSubscribeClick(event) {
