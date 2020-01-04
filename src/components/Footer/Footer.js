5f27cf5fa436208d510f43560ebf7c9a0d856f10
Jochen Kressin <jkressin@floragunn.com>
2020-01-04 18:43:59 +0100

    added org json-ld

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -197,15 +197,55 @@ const Footer = ({ landing }) => {
             t
           </NavLink>
         </div>
-        <script type="application/ld+json">{`
-          {
-            "@context": "https://schema.org",
-            "@type": "Organization",
-            "url": "https://search-guard.com",
-            "logo": "${sgLogo}"
-          }
-        `}</script>
       </div>
+      <script type="application/ld+json">{`
+        {
+          "@context": "http://schema.org",
+          "@type": "Organization",
+          "name": "Search Guard",
+          "legalName" : "floragunn GmbH",
+          "url": "https://search-guard.com",
+          "logo": "${sgLogo}",
+          "foundingDate": "2013",
+          "founders":
+          [
+        {
+          "@type": "Person",
+          "name": "Jochen Kressin"
+        },
+        {
+          "@type": "Person",
+          "name": "Claudia Kressin"
+        }
+          ],
+          "address": {
+          "@type": "PostalAddress",
+          "streetAddress": "Tempelhofer Ufer 16",
+          "addressLocality": "Berlin",
+          "postalCode": "10963",
+          "addressCountry": "Germany"
+        },
+          "contactPoint" : [
+        {
+          "@type": "ContactPoint",
+          "contactType": "sales",
+          "telephone": "[+49-30–89379249]",
+          "email": "sales@search-guard.com"
+        },
+        {
+          "@type": "ContactPoint",
+          "contactType": "customer support",
+          "url": "https://forum.search-guard.com/"
+        }
+          ],
+          "sameAs": [
+          "https://twitter.com/searchguard",
+          "https://www.facebook.com/searchguard/",
+          "https://www.linkedin.com/company/search-guard/",
+          "https://www.youtube.com/channel/UCUw93I0DHMvoA8HNQ31AeJw"
+          ]
+        }
+      `}</script>
     </div>
   );
 };
