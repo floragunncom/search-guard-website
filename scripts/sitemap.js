21c737c7bc00501a917fda61993f76f6f58fc266
Jochen Kressin <jkressin@floragunn.com>
2020-01-10 17:33:31 +0100

    fix sitemap script, add slash

---
+++ scripts/sitemap.js
@@ -0,0 +1,107 @@
+#!/usr/bin/env node
+
+// based on: @leadhome/react-snap-sitemap
+// https://www.npmjs.com/package/@leadhome/react-snap-sitemap
+// License: MIT license
+
+const builder = require('xmlbuilder');
+const { readdir, writeFile, lstat } = require('fs');
+const { promisify } = require('util');
+const args = process.argv;
+
+const preFormattedBaseUrl = args.find((arg) => arg.startsWith('--base-url='));
+const exclusionList = args.find((arg) => arg.startsWith('--black-list='));
+const preFormattedChangeFrequency = args.find((arg) => arg.startsWith('--change-frequency='));
+let blackList = [];
+
+if (exclusionList) {
+    let params = exclusionList.split('=')[1];
+    blackList = params.split(',');
+    console.log('👋 Exluding URLs:', blackList.join(', '))
+}
+
+let baseUrl;
+
+if (preFormattedBaseUrl) {
+    baseUrl  = preFormattedBaseUrl.split('=')[1];
+} else {
+    throw (new Error('Missing valid --base-url command line argument'));
+}
+
+if (baseUrl.length === 0) {
+    throw (new Error('Pass a valid url to --base-url'));
+}
+
+let changeFrequency;
+
+if (preFormattedChangeFrequency) {
+    changeFrequency  = preFormattedChangeFrequency.split('=')[1];
+} else {
+    changeFrequency = 'monthly';
+}
+
+const asyncReaddir = promisify(readdir), asyncWriteFile = promisify(writeFile), asyncLStat = promisify(lstat);
+
+const asyncForEach = async (array, callback) => {
+    for (let index = 0; index < array.length; index++) {
+        await callback(array[index], index, array);
+    }
+}
+
+let allRoots = [];
+
+const readSite = async (dir) => {
+    const directory = await asyncReaddir(dir, { withFileTypes: true });
+
+    await asyncForEach(directory, async fileOrDirectory => {
+        let path = fileOrDirectory;
+
+        if (typeof fileOrDirectory === 'object') {
+            path = fileOrDirectory.name;
+        }
+
+        if (path === '404') {
+            return;
+        }
+
+        const root = `${dir}/${path}`;
+
+        const stat = await asyncLStat(root);
+
+        if (stat.isDirectory()) {
+            await readSite(root);
+        } else if (path === 'index.html') {
+            allRoots = [...allRoots, root];
+        }
+    });
+}
+
+(async () => {
+    await readSite('build');
+
+    const siteUrls = allRoots.map(root => root.replace('build/', baseUrl).replace('/index.html', ''));
+
+    const urlset = builder.create('urlset', { encoding: 'UTF-8', version: '1.0' });
+
+    urlset.attribute('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
+
+    siteUrls
+        .filter(url => !blackList.includes(url))
+        .sort((a, b) => a.length - b.length)
+        .forEach(url => {
+            url = url + "/";
+            const u = urlset.ele('url');
+
+            u.ele('loc', url);
+            if (changeFrequency) {
+                u.ele('changefreq', changeFrequency);
+            }
+            u.ele('priority', 0.5);
+        });
+
+    const sitemap = urlset.end({ pretty: true });
+
+    await asyncWriteFile('build/sitemap.xml', sitemap);
+
+    console.log('🙂 Successfully built sitemap.xml in build directory');
+})();
