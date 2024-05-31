717ac0046bb1737030b8925a2c6faf0e4b59b38f
Jochen Kressin <jkressin@floragunn.com>
2024-05-31 19:29:25 +0200

    cleaned up preview page

--- src/views/Blog/BlogPostArticlePreview.js
+++ src/views/Blog/BlogPostArticlePreview.js
@@ -7,15 +7,11 @@ import BlogPostArticleContent from "../../components/BlogPost/BlogPostArticleCon
 const BlogPostArticlePreview = ({ match }) => {
 
   let params = useParams();
-
-  console.log("Preview");
+  
   const [postContent, setPostContent] = useState(null);
   const [error, setError] = useState(null);
 
   const fetchPost = async() => {
-
-    console.log("this.props.match.params.slug");
-    console.log(params.slug);
 
     const client = require('contentful').createClient({
       space: '95di84mqkkro',
@@ -23,12 +19,9 @@ const BlogPostArticlePreview = ({ match }) => {
       host: 'preview.contentful.com',
     });
 
-    console.log("fetch posts");
-
     // http://localhost:3000/preview/blogpost/kerberos-elasticsearch-searchguard-pt1/
     let posts = await client.getEntries({ content_type: 'post', "fields.slug": params.slug + "/"});
 
-    console.log(posts);
     if (posts && posts.items && posts.items.length > 0) {
       return posts.items[0];
     }
@@ -38,11 +31,9 @@ const BlogPostArticlePreview = ({ match }) => {
   useEffect(() => {
     fetchPost()
         .then(response => {
-          console.log(response);
           setPostContent(response);
         })
         .catch(err => {
-          console.log(err);
           setError(err);
         });
   }, [])
