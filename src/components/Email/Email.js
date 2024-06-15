0050b3a45c62bc80861af9031d93969584b7f80c
Jochen Kressin <jkressin@floragunn.com>
2024-06-15 20:24:06 +0200

    fixed NL signup

--- src/components/Email/Email.js
+++ src/components/Email/Email.js
@@ -23,39 +23,26 @@ const Email = () => {
   }
 
   return (
-    <div>
-      {emailSendStatus ? (
-        <div
-          className="prefooter-content-text bold"
-          style={{ color: '#246E94' }}
-        >
-          Thank you for signing up to our newsletter!
-        </div>
-      ) : (
-        <form onSubmit={onSubscribeClick}>
-          <div className="input-field col s12 m6 l8">
-            <input
-              id="email"
-              name="email"
-              type="email"
-              className="validate"
-              required
-            />
-            <label htmlFor="email" id="email-input">
-              Email address
-            </label>
-            <span
-              className="helper-text"
-              data-error="Please type in the correct format!"
-            />
+          <div className="sendgrid-subscription-widget widget-2963" data-emailerror="Please enter a valid email address" data-nameerror="Please enter your name" data-checkboxerror="Please tick the box to accept our conditions">
+            <form className="sg-widget" data-token="feda1502bd7eb359faa050c8c00fcd14" onSubmit="return false;">
+
+              <div className="row">
+                <div className="col s12 m6 l6">
+                  <input className="sg_email" type="email" name="sg_email" placeholder="Your Email" required="required"></input>
+                </div>
+                <div className="col s12 m6 l6">
+                  <input type="submit" className="sg-submit-btn arrow-button-default-container arrow-button-default-text" id="widget-2963" value="Subscribe"></input>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col s12 m12 l12">
+                  <div className="sg-response"></div>
+                </div>
+              </div>
+            </form>
           </div>
-          <div className="input-field col s12 m6 l4">
-            <Button text="subscribe" buttonStyle="default-button" />
-          </div>
-        </form>
-      )}
-    </div>
   );
 };
+
 
 export default Email;
