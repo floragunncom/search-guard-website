996aeb8e05b03afcc208e244d3c4738256f2d921
Jochen Kressin <jkressin@floragunn.com>
2024-06-25 21:14:59 +0200

    slider for references

--- public/assets/splide-settings.js
+++ public/assets/splide-settings.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-    if ($("#splide-integrators")) {
-        var splide = new Splide( '#splide-integrators', {
+    if ($("#splide-integrators").length) {
+        var splideIntegrators = new Splide( '#splide-integrators', {
                 type   : 'loop',
                 drag   : 'free',
                 focus  : 'center',
@@ -18,6 +18,30 @@ $(document).ready(function(){
                 },
             }
         );
-        splide.mount(window.splide.Extensions);
+        splideIntegrators.mount(window.splide.Extensions);
+    }
+
+    console.log($("#splide-references"));
+
+    if ($("#splide-references").length) {
+        var splideReferences = new Splide( '#splide-references', {
+                type   : 'loop',
+                drag   : 'free',
+                focus  : 'center',
+                pauseOnHover: false,
+                autoStart: true,
+                autoWidth: true,
+                perPage: 10,
+                lazyload: true,
+                arrows: false,
+                pagination: false,
+                autoScroll: {
+                    speed: 0.6,
+                    pauseOnHover: false,
+                    pauseOnFocus: false
+                },
+            }
+        );
+        splideReferences.mount(window.splide.Extensions);
     }
 });
