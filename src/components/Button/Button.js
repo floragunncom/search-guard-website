e0c794c403cea79f65ab0120e5b8023698d3f973
Jochen Kressin <jochen.kressin@eliatra.com>
2025-01-09 22:05:59 +0100

    reformatted code

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -9,45 +9,48 @@ import ButtonSand from './ButtonSand';
 import './Button.scss';
 
 const Button = props => {
-console.log(props.buttonStyle);
-  if (!props.buttonStyle) {
-    return <ButtonLightGreen text={props.text} onPress={props.onPress} target={props.target} link={props.link}/>;
-  }
 
-  // REMOVE UNUSED BUTTONS
-  switch (props.buttonStyle) {
-    case 'loud-button':
-      return <ButtonLoud text={props.text} onPress={props.onPress} />;
-    case 'default-button':
-      return <ButtonDefault text={props.text} onPress={props.onPress} />;
-    case 'light-green-button':
-      return <ButtonLightGreen text={props.text} onPress={props.onPress} target={props.target} link={props.link}/>;      
-    case 'loud-link':
-      return (
-        <LinkLoud text={props.text} target={props.target} link={props.link} />
-      );
-    case 'sand-button':
-      return <ButtonSand text={props.text} target={props.target} link={props.link} />;
-    case 'default-link':
-      return (
-        <LinkDefault
-          text={props.text}
-          target={props.target}
-          link={props.link}
-        />
-      );
-    case 'ghost-link':
-      return (
-        <LinkGhost
-          text={props.text}
-          target={props.target}
-          link={props.link}
-          color={props.color}
-        />
-      );
-    default:
-      return <ButtonLightGreen text={props.text} onPress={props.onPress} target={props.target} link={props.link}/>;
-  }
+    // default
+    if (!props.buttonStyle) {
+        return <ButtonLightGreen text={props.text} onPress={props.onPress} target={props.target} link={props.link}/>;
+    }
+
+    // REMOVE UNUSED BUTTONS
+    switch (props.buttonStyle) {
+        case 'loud-button':
+            return <ButtonLoud text={props.text} onPress={props.onPress}/>;
+        case 'default-button':
+            return <ButtonDefault text={props.text} onPress={props.onPress}/>;
+        case 'light-green-button':
+            return <ButtonLightGreen text={props.text} onPress={props.onPress} target={props.target}
+                                     link={props.link}/>;
+        case 'loud-link':
+            return (
+                <LinkLoud text={props.text} target={props.target} link={props.link}/>
+            );
+        case 'sand-button':
+            return <ButtonSand text={props.text} target={props.target} link={props.link}/>;
+        case 'default-link':
+            return (
+                <LinkDefault
+                    text={props.text}
+                    target={props.target}
+                    link={props.link}
+                />
+            );
+        case 'ghost-link':
+            return (
+                <LinkGhost
+                    text={props.text}
+                    target={props.target}
+                    link={props.link}
+                    color={props.color}
+                />
+            );
+        default:
+            return <ButtonLightGreen text={props.text} onPress={props.onPress} target={props.target}
+                                     link={props.link}/>;
+    }
 };
 
 export default Button;
