04b86850815e65f18aa81756040a1f331722b5ec
Jochen Kressin <jochen.kressin@eliatra.com>
2025-01-09 22:26:29 +0100

    cleaned up colors

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,35 +1,24 @@
 import React from 'react';
-import LinkLoud from './LinkLoud';
-import ButtonLoud from './ButtonLoud';
 import ButtonLightGreen from './ButtonLightGreen';
 import ButtonDefault from './ButtonDefault';
 import LinkDefault from './LinkDefault';
 import LinkGhost from './LinkGhost';
-import ButtonSand from './ButtonSand';
 import './Button.scss';
 
 const Button = props => {
 
     // default
     if (!props.buttonStyle) {
-        return <ButtonLightGreen text={props.text} onPress={props.onPress} target={props.target} link={props.link}/>;
+        props.buttonStyle = 'light-green-button';
     }
 
     // REMOVE UNUSED BUTTONS
     switch (props.buttonStyle) {
-        case 'loud-button':
-            return <ButtonLoud text={props.text} onPress={props.onPress}/>;
         case 'default-button':
             return <ButtonDefault text={props.text} onPress={props.onPress}/>;
         case 'light-green-button':
             return <ButtonLightGreen text={props.text} onPress={props.onPress} target={props.target}
                                      link={props.link}/>;
-        case 'loud-link':
-            return (
-                <LinkLoud text={props.text} target={props.target} link={props.link}/>
-            );
-        case 'sand-button':
-            return <ButtonSand text={props.text} target={props.target} link={props.link}/>;
         case 'default-link':
             return (
                 <LinkDefault
