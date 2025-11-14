9420d1c650fb53da9416abbca770563e4db8e1fb
Anton Rubin <anton.rubin@eliatra.com>
2025-11-14 16:47:47 +0000

    typo fix for signals

--- src/views/Advisory/Advisory.js
+++ src/views/Advisory/Advisory.js
@@ -28,7 +28,7 @@ const bugs = [
         ),
         date: '2025-11-14',
         vul:
-            'When the search is trigged from signal\'s watch, the DLS rule is not enforced, allowing access to all documents in the queried indices',
+            'When the search is trigged from Signals watch, the DLS rule is not enforced, allowing access to all documents in the queried indices',
         rem: 'Update',
         fix: 'FLX3.1.3',
         report: 'floragunn',
