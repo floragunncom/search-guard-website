b044f1a968794f5b1576453c6b352699f26a4ca2
Jochen Kressin <jochen.kressin@eliatra.com>
2026-08-14 14:28:26 +0200

    removed duplicate cookie script; added MS Clarity (experimental)

--- pages/_document.js
+++ pages/_document.js
@@ -209,11 +209,6 @@ class MyDocument extends Document {
 
           <link rel="stylesheet" href="/assets/materialize.min.css" />
           <link rel="stylesheet" href="/assets/fonts/fonts.css" />
-          <script
-            type="text/javascript"
-            src="https://cdn.cookie-script.com/s/a521e590130162749ba95a6c3ffc0462.js"
-            async
-          />
 
           {/* Plausible Analytics — cookieless, no consent required */}
           <script
@@ -237,6 +232,15 @@ class MyDocument extends Document {
             dangerouslySetInnerHTML={{
               __html:
                 "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TDLZ33C');",
+            }}
+          />
+          {/* Microsoft Clarity — gated behind Cookie-Script "performance" consent. */}
+          <script
+            type="text/plain"
+            data-cookiecategory="performance"
+            dangerouslySetInnerHTML={{
+              __html:
+                "(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src='https://www.clarity.ms/tag/'+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window,document,'clarity','script','y281cr4x0u');",
             }}
           />
         </Head>
